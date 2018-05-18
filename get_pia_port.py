ca558870131d5dba8b37921da3869254b00e0d71
Christopher Mastrogiuseppe <chris.mastrogiuseppe@gmail.com>
2018-05-17 19:23:58 -0700

    Create Initial draft of get pia port python script

---
+++ get_pia_port.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import requests
+import netifaces as ni
+from hashlib import md5
+import os
+import json
+
+USERNAME = ''
+PASSWORD = ''
+
+def get_interface_ip(iface):
+    ni.ifaddresses(iface)
+    return ni.ifaddresses(iface)[ni.AF_INET][0]['addr']
+
+def get_pia_port(username, password, iface):
+    ip = get_interface_ip(iface)
+    client_id = md5(os.urandom(16)).hexdigest()
+    payload = { 'client_id': client_id, 
+                'local_ip': ip,
+                'user': USERNAME, 
+                'pass': PASSWORD }
+    r = requests.post('https://www.privateinternetaccess.com/vpninfo/port_forward_assignment', data=payload)
+    port_json = json.loads(r.text)
+    return port_json['port']
+
+print(get_pia_port(USERNAME,PASSWORD,'tun0'))
